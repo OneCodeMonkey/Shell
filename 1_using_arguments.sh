0b0b0a2ddf0e296af9bcb963a86806b98d5dc348
OneCodeMonkey <1460018362@qq.com>
2019-04-27 16:42:13 +0000

    add bash: using shell arguments

---
+++ 1_using_arguments.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+#example of using arguments to a script
+echo "My first argument is $1"
+echo "My second argument is $2"
+echo "My third argument is $3"
+echo "My other arguments are $4 $5 $6 $7 $8 $9 ${10} ${11}"
+echo "Total amount of arguments is $#"
