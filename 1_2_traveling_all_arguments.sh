da130a1605ea334b5683d259a5aca66375ff6286
OneCodeMonkey <1460018362@qq.com>
2019-04-27 17:11:48 +0000

    add explanation

--- 1_2_traveling_all_arguments.sh
+++ 1_2_traveling_all_arguments.sh
@@ -3,7 +3,7 @@
 # travel through all input arguments
 echo -e "Now total arguments is $#.\nAnd every one of them is the following:"
 count=1;step=1
-while (($# > 0))
+while (($# > 0))  # 注意这里循环体，while或者for ，这里的判断体要用两层括号包围来写，因为单层括号会被解释成【提高运算符优先级的普通括号】
 do
 echo -e "Argument $count : $1"
 count=`expr $count + $step`
