207aa2799e57bea13256239db98f0f11033b9557
OneCodeMonkey <1460018362@qq.com>
2019-06-13 00:36:36 +0800

    fix

--- utils/reverse_array.sh
+++ utils/reverse_array.sh
@@ -1,6 +1,6 @@
 reverse_array() {
 	# Usage: reverse_array "array"
 	shopt -s extdebug
-	f()(printf '%s\n' "${BASH_ARGV[@]}");f "$@"
+	f()(printf '%s\n' "${BASH_ARGV[@]}"); f "$@"
 	shopt -u extdebug
 }
