f945d40cbc217a373866c2d5514d4f97b42c69f8
OneCodeMonkey <1460018362@qq.com>
2019-06-14 00:15:21 +0800

    add another alternative method

--- utils/file/count_lines.sh
+++ utils/file/count_lines.sh
@@ -1,6 +1,16 @@
 # get the count of lines of a file
 line_count() {
-	# Usage: lines "file"
+	# Usage: line_count "file"
 	mapfile -tn 0 lines < "$1"
 	printf '%s\n' "${#lines[@]}"
+}
+
+# another altertive in bash<=3
+line_count_2() {
+	# Usage: line_count_2 "file"
+	count = 0
+	while IFS= read -r _; do
+		((count++))
+	done < "$1"
+	printf '%s\n' "$count"
 }
