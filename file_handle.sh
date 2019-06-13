35960acce307b556fb9b2e8add2e748909010279
OneCodeMonkey <1460018362@qq.com>
2019-06-14 00:07:47 +0800

    read a file to array by line

--- file_handle.sh
+++ file_handle.sh
@@ -1,2 +1,8 @@
 # read a file to string
 file_data = "$(<"file")"
+
+# read a file to an array by line
+# if Bash version<4
+IFS = $'\n' read -d "" -ra file_data < "file"
+# if Bash version>=4
+mapfile -t file_data < "file"
