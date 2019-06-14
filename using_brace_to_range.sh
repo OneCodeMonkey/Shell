06e9b1ff6b94f01b21553acc614889b23319b869
OneCodeMonkey <1460018362@qq.com>
2019-06-14 18:41:32 +0800

    using brace to get complex ranges

---
+++ using_brace_to_range.sh
@@ -0,0 +1,21 @@
+# for ranges
+
+# print numbers 1-100
+echo {1..100}
+
+# print range of floats 1.1 - 1.9
+echo 1.{1..9}
+
+# print chars a to z
+echo {a..z}
+echo {A..Z}
+
+# nesting
+echo {A..Z}{0..9}
+
+# print zero-padded numbers 001,002,..,010,011,..,100
+echo {01..100}
+
+# change increment amount
+# for example, increment by 2
+echo {1..10..2}
